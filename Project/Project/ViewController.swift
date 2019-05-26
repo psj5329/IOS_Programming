13efa599031bf09319e93e28afff42165a3b2bf7
kpugame <kpugame@kpugameui-MacBook-Pro-5.local>
2019-05-26 20:48:21 +0900

    190526 프로젝트 생성

---
+++ Project/Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by kpugame on 2019. 5. 26..
+//  Copyright © 2019년 SooJeong Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
